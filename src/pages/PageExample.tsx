0b45de5c1122e066dd9c706a14560c332e744ebb
IAIAI-aii <aisyahtijan@gmail.com>
2026-04-27 15:57:54 +0700

    fix error

--- src/pages/PageExample.tsx
+++ src/pages/PageExample.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from "@/components/Navbar";
 import HeaderCarousel from "@/components/HeaderCarousel";
 import FeaturesSection from "@/components/FeaturesSection";
@@ -21,12 +20,4 @@ export default function PageExample() {
         <Footer />
     </main>
     );
-=======
-export default function PageExample() {
-  return (
-    <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
-      <h1>Page Example</h1>
-    </div>
-  );
->>>>>>> a7c68a7cbfbb66225cd77a48b0904e4bd20cef88
 }
