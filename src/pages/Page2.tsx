3353455f0a8b448bebe389577939aaecc40ced7d
adheliaissabel <h1101241001@student.untan.ac.id>
2026-04-27 14:54:38 +0700

    feat: add pricing cards and toggle button page2

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,7 +1,9 @@
+import PricingCalculator from "../components/PricingCalculator";
+
 export default function Page2() {
     return (
-        <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
-            <h1>Page 2</h1>
+        <div className="min-h-screen bg-white">
+            <PricingCalculator />
         </div>
     )
 }
