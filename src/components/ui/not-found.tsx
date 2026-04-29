0f14153cdb042b5c32b24e2f3cb587dd558ff9dd
Iqlima Nurin <h1101241007student.untan.ac.id>
2026-04-29 21:45:15 +0700

    chore: remove components, hooks, lib, styles, router, routeTree (orang 3 only)

--- src/components/ui/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export function NotFound() {
-  return (
-    <div className="flex items-center justify-center h-screen w-screen overflow-hidden">
-      <div className="text-sm font-bold">404 - Not Found</div>
-    </div>
-  );
-}
-
