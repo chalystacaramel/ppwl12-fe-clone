2623c7849bb2ae2e40bac22921cbc9745b549476
IAIAI-aii <aisyahtijan@gmail.com>
2026-04-29 23:35:21 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,44 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+// vite.config.ts
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
+import { viteComponentMapper } from 'step1-tagger';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
-  },
   server: {
     port: 5173,
     strictPort: true
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    viteComponentMapper(),
+    tanstackStart(),
+    nitro({
+      output: {
+        dir: 'dist',
+      },
+    }),
+    // react's vite plugin must come after start's vite plugin
+    viteReact(),
+    tailwindcss(),
+  ],
+  nitro: {
+    preset: 'deno-deploy',
+  },
+  build: {
+    sourcemap: 'hidden',
+    rollupOptions: {
+      external(source) {
+        if (source.startsWith('node:')) {
+          return true;
+        }
+        return false;
+      },
+    },
   },
 });
